ee966ad7b92b249a41b3908ff874c745c40e9c05
Gaurav Kumar <gauravkumar260607@gmail.com>
2026-09-20 09:39:51 +0530

    Update API_BASE URL in script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const API_BASE = "https://mental-health-analyzer-zqlp.onrender.com/predict";
+  const API_BASE = "https://mental-health-analyzer-zqlp.onrender.com/";
 
   const form = document.getElementById("predict-form");
   const submitBtn = document.getElementById("submit-btn");
